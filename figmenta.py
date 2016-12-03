798372886c45ba431134bd7c68395adff1df1ce8
Justin Li <justinnhli@gmail.com>
2016-12-02 23:40:23 -0800

    initial commit

---
+++ figmenta.py
@@ -0,0 +1,7 @@
+from bokeh.plotting import figure, ColumnDataSource
+from bokeh.models import HoverTool
+
+def autovis(df, ys, xs=None, **kwargs):
+    x_dim = len(xs)
+    y_dim = len(ys)
+    pass
